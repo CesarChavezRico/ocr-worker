a282532319a9ccaaae2bf5dd02b661e5321952c0
Cesar <cesar@golocky.com>
2015-04-09 15:14:11 -0500

    functionality to run on GCE

--- picture.py
+++ picture.py
@@ -13,6 +13,7 @@ from PIL import Image
 class Picture():
 
     pic = Image
+    cropped = Image
     reading_id = ''
 
     def __init__(self, reading_id):
@@ -29,8 +30,48 @@ class Picture():
             resp = r.execute()
             self.pic = Image.open(StringIO(resp))
             self.reading_id = reading_id
+            self.extract_region_of_interest()
         else:
             print 'Error getting credentials'
+
+    def extract_region_of_interest(self):
+        """
+        Crops self.pic to extract the region of interest.
+        Crop size is determined as a function of the image size, type of image (red, green, blue .. see CL photo app)
+        and the relative size of the region of interest in both X and Y. It is also assumed that the region of interest
+        is in the approximate center of the original image.
+            :return: (None) stores the new image in self.cropped
+        """
+        redYpercent = .07
+        redXpercent = .45
+
+        blueYpercent = .09
+        blueXpercent = .55
+
+        greenYpercent = .15
+        greenXpercent = .65
+
+        # Determine cut size based on type of image
+        # cut_size
+        if 'red' in self.reading_id:
+            text = 'red'
+            cut_size = (self.pic.size[0]*redXpercent, self.pic.size[1]*redYpercent)
+        elif 'blue' in self.reading_id:
+            text = 'blue'
+            cut_size = (self.pic.size[1]*blueYpercent, self.pic.size[0]*blueXpercent)
+        elif 'green' in self.reading_id:
+            text = 'green'
+            cut_size = (self.pic.size[1]*greenYpercent, self.pic.size[0]*greenXpercent)
+
+        print ('Image Size ({2}) = {0}, {1}'.format(self.pic.size[1], self.pic.size[0], text))
+
+        # order in which we specify the coordinates is:
+        # startY:startX, endY:endX
+
+        image_center = self.pic.size[0]/2, self.pic.size[1]/2
+        box = ((image_center[1]-cut_size[1]/2), (image_center[0]-cut_size[0]/2),
+               (image_center[1]+cut_size[1]/2), (image_center[0]+cut_size[0]/2))
+        self.cropped = self.pic.crop(box)
 
     def get_reading_id(self):
         """
