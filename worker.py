37f5960ca0840979be4429c68079936cc77ee0ad
Cesar <cesar@golocky.com>
2015-05-05 18:40:58 -0500

    first commit of result notification to app engine. --non functional

--- worker.py
+++ worker.py
@@ -5,6 +5,7 @@ __author__ = 'cesar'
 
 from picture import Picture
 from detection import Detection
+from apiclient.discovery import build
 import requests
 
 VMX_SERVER = 'http://vmx-server.ddns.net:80'
@@ -46,6 +47,16 @@ class Worker():
             for num in sorted(number, key=lambda n: n.center):
                 reading += str(num.value)
             print 'Number in image [{0}]: {1}'.format(self.pic.get_public_url(), reading)
+            try:
+                r = self._post_result_to_app_engine()
+                if r:
+                    print 'Result successfully posted to AppEngine!'
+                else:
+                    print '->>> Something is wrong!'
+            except Exception as e:
+                # TODO: Retry request ..
+                print 'Can not post result to AppEngine, retry task!'
+                print e.__str__()
 
         else:
             print(r.status_code)
@@ -56,3 +67,24 @@ class Worker():
         the task queue after successfully updating the reading entity on the datastore
             :return: True if success, Exception otherwise
         """
+        api_root = 'https://ocr-backend.appspot.com/_ah/api'
+        api = 'backend'
+        version = 'v1'
+        discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (api_root, api, version)
+        print discovery_url
+        backend = build(api, version, discoveryServiceUrl=discovery_url)
+
+        payload = {"error": "",
+                   "result": result,
+                   "task_name": self.task_name,
+                   "task_payload": self.task_payload
+                   }
+
+        request = backend.meter().create(body=payload)
+        response = request.execute()
+        print response
+        if response['ok'] == 'true':
+            return True
+        else:
+            raise Exception
+
